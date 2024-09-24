efc16917de4316364c61c4ab9903e15af05a5dd6
Angelica2437 <m.moralesangelica@gmail.com>
2024-09-24 18:31:37 +0200

    added moment.js and timezone

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,5 @@
-
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+losAngelesTimeElement.innerHTML = "6:20:15 <small>AM</small>";
