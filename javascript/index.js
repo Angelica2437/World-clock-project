4b60d673bc2e60265f876a0656fb15dedaa9bd7b
Angelica2437 <m.moralesangelica@gmail.com>
2024-09-25 15:38:00 +0200

    created code for one city to test it

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,13 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-losAngelesTimeElement.innerHTML = "6:20:15 <small>AM</small>";
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+}
+updateTime();
+setInterval(updateTime, 1000);
