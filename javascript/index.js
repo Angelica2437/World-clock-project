c204d8db02ba4456140eeface16ebcb5e2eda663
Angelica2437 <m.moralesangelica@gmail.com>
2024-09-26 15:09:37 +0200

    added the select functionality

--- javascript/index.js
+++ javascript/index.js
@@ -8,6 +8,39 @@ function updateTime() {
     "h:mm:ss [<small>]A[</small>]"
   );
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+  <div>
+  <h2>${cityName}</h2>
+  <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+  </div>
+  <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small>
+  </div>
+  </div>
+`;
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+//dropdown
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
